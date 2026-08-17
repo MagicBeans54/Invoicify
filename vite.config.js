0d6a8586091d199d6ae19e305ad9edd256d62674
Vincent Cruz <vincentcruz54@gmail.com>
2026-08-17 14:49:42 +0800

    YES YES YES

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,28 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
-        react(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.jsx'],
             refresh: true,
         }),
         tailwindcss(),
     ],
+    esbuild: {
+        loader: 'jsx',
+        include: /.*\.jsx$/,
+        exclude: /node_modules/,
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            loader: {
+                '.js': 'jsx',
+                '.jsx': 'jsx',
+            },
+        },
+    },
     server: {
         watch: {
             ignored: ['**/storage/framework/views/**'],
