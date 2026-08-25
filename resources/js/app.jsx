cee89f082c246251ca79309b49e3c103f62b24ce
dreamerrri <114892885+dreamerrri@users.noreply.github.com>
2026-08-25 10:00:15 +0800

    title fix

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,7 +4,8 @@ import { createInertiaApp } from '@inertiajs/react';
 import { createRoot, hydrateRoot } from 'react-dom/client';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName =
+    document.querySelector('meta[name="app-name"]')?.content || 'Invoicify';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
