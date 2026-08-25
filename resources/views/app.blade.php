e9cbcc0f3134aa6f1fe72b4d3a255cec6c1bfc0b
dreamerrri <114892885+dreamerrri@users.noreply.github.com>
2026-08-25 09:48:17 +0800

    updated title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title inertia>{{ $page['props']['title'] ?? config('app.name', 'Laravel') }}</title>
+        <title>Invoicify</title>
         @vite(['resources/css/app.css', 'resources/js/app.jsx'])
         @inertiaHead
         @routes
