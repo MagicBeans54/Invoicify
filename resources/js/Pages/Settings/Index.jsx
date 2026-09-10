f334e81bf08d56c787ba66de500ad8c73ceb92b0
dreamerrri <114892885+dreamerrri@users.noreply.github.com>
2026-09-10 10:09:09 +0800

    minor tweak on settings page

--- resources/js/Pages/Settings/Index.jsx
+++ resources/js/Pages/Settings/Index.jsx
@@ -49,7 +49,7 @@ export default function Index({ settings }) {
         <>
             <Head title="Company Settings" />
             <AppLayout title="Settings">
-                <div className="mx-auto max-w-3xl space-y-6">
+                <div>
                     <form onSubmit={handleSubmit}>
                         <Card>
                             <CardHeader>
